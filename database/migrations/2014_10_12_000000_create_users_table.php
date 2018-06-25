2f816e92418b47c92e39aada9705d05aa358cd45
sammymwangangi <sammymwinzi2@gmail.com>
2018-06-25 15:49:58 +0300

    auth scaffolding

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,17 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->integer('national_id')->unique();
+            $table->integer('phone_number')->unique();
+            $table->integer('kra')->unique();
+            $table->integer('date_registered');
+            $table->integer('date_of_birth');
+            $table->string('gender');
+            $table->string('account_type');
+            $table->string('marital_status');
+            $table->string('postal_address');
+            $table->string('public_address');
+            $table->boolean('admin')->default(0);
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
